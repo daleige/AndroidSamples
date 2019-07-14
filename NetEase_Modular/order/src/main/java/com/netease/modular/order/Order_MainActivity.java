697df15a8c6aa8552e94917cff4da218992b6c1a
mutou <766790183@qq.com>
2019-07-14 23:31:41 +0800

    gradle

---
+++ NetEase_Modular/order/src/main/java/com/netease/modular/order/Order_MainActivity.java
@@ -0,0 +1,13 @@
+package com.netease.modular.order;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Order_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_activity_main);
+    }
+}
