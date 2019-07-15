20e035dceac42255dd9614da95622ed2c9f1f8b6
mutou <766790183@qq.com>
2019-07-15 15:03:33 +0800

    personal module

---
+++ NetEase_Modular/personal/src/main/java/com/netease/modular/personal/Personal_MainActivity.java
@@ -0,0 +1,13 @@
+package com.netease.modular.personal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Personal_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_activity_main);
+    }
+}
