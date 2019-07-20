5a4de880f60461bef61638c36c36219a97d2cd74
mutou <766790183@qq.com>
2019-07-21 02:04:11 +0800

    apt

---
+++ NetEase_Modular_APT/app/src/main/java/com/netease/modular/apt/OrderActivity.java
@@ -0,0 +1,12 @@
+package com.netease.modular.apt;
+
+import com.netease.modular.annotation.ARouter;
+
+/**
+ * Create by 陈扬齐
+ * Create on 2019-07-21
+ * description:
+ */
+@ARouter(path = "/app/OrderActivity")
+public class OrderActivity {
+}
--- NetEase_Modular_APT/app/src/main/java/com/netease/modular/apt/PersonalActivity.java
+++ NetEase_Modular_APT/app/src/main/java/com/netease/modular/apt/PersonalActivity.java
@@ -1,23 +1,23 @@
 package com.netease.modular.apt;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 
 import com.netease.modular.annotation.ARouter;
 
-@ARouter(path = "/app/MainActivity")
-public class MainActivity extends AppCompatActivity {
+@ARouter(path = "/app/PersonalActivity")
+public class PersonalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_personal);
     }
 
     public void jump(View view) {
-        Class<?> targetClass=PersonalActivity$$ARouter.findTargetClass("/app/PersonalActivity");
+        Class<?> targetClass=MainActivity$$ARouter.findTargetClass("/app/MainActivity");
         startActivity(new Intent(this,targetClass));
     }
 }
