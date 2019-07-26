fffd2ba6926b5f8b9df21aff9831db4acff8b0dd
mutou <766790183@qq.com>
2019-07-27 02:21:38 +0800

    修改

--- MusicPlayer/app/src/main/java/com/cyq/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/cyq/musicplayer/MainActivity.java
@@ -1,13 +1,37 @@
 package com.cyq.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setOverflowIcon(ContextCompat.getDrawable(this, R.drawable.more));
+        ViewGroup contentFrameLayout = findViewById(android.R.id.content);
+        View parentView = contentFrameLayout.getChildAt(0);
+        if (parentView != null) {
+            parentView.setFitsSystemWindows(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_music, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
---
+++ MusicPlayer/app/src/main/java/com/cyq/musicplayer/view/StatusBarView.java
@@ -0,0 +1,24 @@
+package com.cyq.musicplayer.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Create by 陈扬齐
+ * Create on 2019-07-27
+ * description:
+ */
+public class StatusBarView extends View {
+    public StatusBarView(Context context) {
+        super(context);
+    }
+
+    public StatusBarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StatusBarView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
