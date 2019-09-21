c33e8bacfdfc725ede95e5f0cb7c1b2935b06702
mutou <766790183@qq.com>
2019-09-21 15:18:36 +0800

    bitmap资源封装

---
+++ CustomGlide/app/src/main/java/com/cyq/customglide/Tool.java
@@ -0,0 +1,355 @@
+package com.cyq.customglide;
+
+import android.annotation.TargetApi;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Time: 2019-09-21 14:51
+ * Author: ChenYangQi
+ * Description:
+ */
+public class Tool {
+    private static final int HASH_MULTIPLIER = 31;
+    private static final int HASH_ACCUMULATOR = 17;
+    private static final char[] HEX_CHAR_ARRAY = "0123456789abcdef".toCharArray();
+    // 32 bytes from sha-256 -> 64 hex chars.
+    private static final char[] SHA_256_CHARS = new char[64];
+
+    private Tool() {
+        // Utility class.
+    }
+
+    /**
+     * Returns the hex string of the given byte array representing a SHA256 hash.
+     */
+    @NonNull
+    public static String sha256BytesToHex(@NonNull byte[] bytes) {
+        synchronized (SHA_256_CHARS) {
+            return bytesToHex(bytes, SHA_256_CHARS);
+        }
+    }
+
+    // Taken from:
+    // http://stackoverflow.com/questions/9655181/convert-from-byte-array-to-hex-string-in-java
+    // /9655275#9655275
+    @SuppressWarnings("PMD.UseVarargs")
+    @NonNull
+    private static String bytesToHex(@NonNull byte[] bytes, @NonNull char[] hexChars) {
+        int v;
+        for (int j = 0; j < bytes.length; j++) {
+            v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_CHAR_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_CHAR_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    /**
+     * Returns the allocated byte size of the given bitmap.
+     *
+     * @see #getBitmapByteSize(Bitmap)
+     * @deprecated Use {@link #getBitmapByteSize(Bitmap)} instead. Scheduled to be
+     * removed in Glide 4.0.
+     */
+    @Deprecated
+    public static int getSize(@NonNull Bitmap bitmap) {
+        return getBitmapByteSize(bitmap);
+    }
+
+    /**
+     * Returns the in memory size of the given {@link Bitmap} in bytes.
+     */
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    public static int getBitmapByteSize(@NonNull Bitmap bitmap) {
+        // The return value of getAllocationByteCount silently changes for recycled bitmaps from the
+        // internal buffer size to row bytes * height. To avoid random inconsistencies in caches, we
+        // instead assert here.
+        if (bitmap.isRecycled()) {
+            throw new IllegalStateException("Cannot obtain size for recycled Bitmap: " + bitmap
+                    + "[" + bitmap.getWidth() + "x" + bitmap.getHeight() + "] " + bitmap.getConfig());
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // Workaround for KitKat initial release NPE in Bitmap, fixed in MR1. See issue #148.
+            try {
+                return bitmap.getAllocationByteCount();
+            } catch (@SuppressWarnings("PMD.AvoidCatchingNPE") NullPointerException e) {
+                // Do nothing.
+            }
+        }
+        return bitmap.getHeight() * bitmap.getRowBytes();
+    }
+
+    /**
+     * Returns the in memory size of {@link Bitmap} with the given width, height, and
+     * {@link Bitmap.Config}.
+     */
+    public static int getBitmapByteSize(int width, int height, @Nullable Bitmap.Config config) {
+        return width * height * getBytesPerPixel(config);
+    }
+
+    private static int getBytesPerPixel(@Nullable Bitmap.Config config) {
+        // A bitmap by decoding a GIF has null "config" in certain environments.
+        if (config == null) {
+            config = Bitmap.Config.ARGB_8888;
+        }
+
+        int bytesPerPixel;
+        switch (config) {
+            case ALPHA_8:
+                bytesPerPixel = 1;
+                break;
+            case RGB_565:
+            case ARGB_4444:
+                bytesPerPixel = 2;
+                break;
+            case RGBA_F16:
+                bytesPerPixel = 8;
+                break;
+            case ARGB_8888:
+            default:
+                bytesPerPixel = 4;
+                break;
+        }
+        return bytesPerPixel;
+    }
+
+    /**
+     * Throws an {@link IllegalArgumentException} if called on a thread other than the main
+     * thread.
+     */
+    public static void assertMainThread() {
+        if (!isOnMainThread()) {
+            throw new IllegalArgumentException("You must call this method on the main thread");
+        }
+    }
+
+    /**
+     * Throws an {@link IllegalArgumentException} if called on the main thread.
+     */
+    public static void assertBackgroundThread() {
+        if (!isOnBackgroundThread()) {
+            throw new IllegalArgumentException("You must call this method on a background thread");
+        }
+    }
+
+    /**
+     * Returns {@code true} if called on the main thread, {@code false} otherwise.
+     */
+    public static boolean isOnMainThread() {
+        return Looper.myLooper() == Looper.getMainLooper();
+    }
+
+    /**
+     * Returns {@code true} if called on a background thread, {@code false} otherwise.
+     */
+    public static boolean isOnBackgroundThread() {
+        return !isOnMainThread();
+    }
+
+    /**
+     * Creates a {@link Queue} of the given size using Glide's preferred implementation.
+     */
+    @NonNull
+    public static <T> Queue<T> createQueue(int size) {
+        return new ArrayDeque<>(size);
+    }
+
+    /**
+     * Returns a copy of the given list that is safe to iterate over and perform actions that may
+     * modify the original list.
+     *
+     * <p>See #303, #375, #322, #2262.
+     */
+    @NonNull
+    @SuppressWarnings("UseBulkOperation")
+    public static <T> List<T> getSnapshot(@NonNull Collection<T> other) {
+        // toArray creates a new ArrayList internally and does not guarantee that the values it
+        // contains
+        // are non-null. Collections.addAll in ArrayList uses toArray internally and therefore also
+        // doesn't guarantee that entries are non-null. WeakHashMap's iterator does avoid
+        // returning null
+        // and is therefore safe to use. See #322, #2262.
+        List<T> result = new ArrayList<>(other.size());
+        for (T item : other) {
+            if (item != null) {
+                result.add(item);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Null-safe equivalent of {@code a.equals(b)}.
+     *
+     * @see java.util.Objects#equals
+     */
+    public static boolean bothNullOrEqual(@Nullable Object a, @Nullable Object b) {
+        return a == null ? b == null : a.equals(b);
+    }
+
+    public static int hashCode(int value) {
+        return hashCode(value, HASH_ACCUMULATOR);
+    }
+
+    public static int hashCode(int value, int accumulator) {
+        return accumulator * HASH_MULTIPLIER + value;
+    }
+
+    public static int hashCode(float value) {
+        return hashCode(value, HASH_ACCUMULATOR);
+    }
+
+    public static int hashCode(float value, int accumulator) {
+        return hashCode(Float.floatToIntBits(value), accumulator);
+    }
+
+    public static int hashCode(@Nullable Object object, int accumulator) {
+        return hashCode(object == null ? 0 : object.hashCode(), accumulator);
+    }
+
+    public static int hashCode(boolean value, int accumulator) {
+        return hashCode(value ? 1 : 0, accumulator);
+    }
+
+    public static int hashCode(boolean value) {
+        return hashCode(value, HASH_ACCUMULATOR);
+    }
+
+
+    // >>>>>>>>> Preconditions
+
+    public static void checkArgument(boolean expression, @NonNull String message) {
+        if (!expression) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    @NonNull
+    public static <T> T checkNotNull(@Nullable T arg) {
+        return checkNotNull(arg, "Argument must not be null");
+    }
+
+    @NonNull
+    public static <T> T checkNotNull(@Nullable T arg, @NonNull String message) {
+        if (arg == null) {
+            throw new NullPointerException(message);
+        }
+        return arg;
+    }
+
+    @NonNull
+    public static String checkNotEmpty(@Nullable String string) {
+        if (TextUtils.isEmpty(string)) {
+            throw new IllegalArgumentException("Must not be null or empty");
+        }
+        return string;
+    }
+
+    @NonNull
+    public static <T extends Collection<Y>, Y> T checkNotEmpty(@NonNull T collection) {
+        if (collection.isEmpty()) {
+            throw new IllegalArgumentException("Must not be empty. 传递进来的值:" + collection + "是null");
+        }
+        return collection;
+    }
+
+    public static void checkNotEmpty(Bitmap bitmap) {
+        if (null == bitmap) {
+            throw new IllegalArgumentException("Must not be empty. 传递进来的值bitmap:" + bitmap +
+                    "是null");
+        }
+    }
+
+
+    // >>>>>>>>>>.
+
+    /**
+     * 利用java原生的摘要实现SHA256加密
+     *
+     * @param str 加密后的报文
+     * @return
+     */
+    public static String getSHA256StrJava(String str) {
+        MessageDigest messageDigest;
+        String encodeStr = "";
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(str.getBytes("UTF-8"));
+            encodeStr = byte2Hex(messageDigest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return encodeStr;
+    }
+
+    /**
+     * 将byte转为16进制
+     *
+     * @param bytes
+     * @return
+     */
+    private static String byte2Hex(byte[] bytes) {
+        StringBuffer stringBuffer = new StringBuffer();
+        String temp = null;
+        for (int i = 0; i < bytes.length; i++) {
+            temp = Integer.toHexString(bytes[i] & 0xFF);
+            if (temp.length() == 1) {
+                //1得到一位的进行补0操作
+                stringBuffer.append("0");
+            }
+            stringBuffer.append(temp);
+        }
+        return stringBuffer.toString();
+    }
+
+    public static void checkNotEmpty(ImageView imageView) {
+        if (imageView == null) {
+            throw new IllegalArgumentException("Must not be empty. 传递进来的值imageView:" + imageView + "是null");
+        }
+    }
+
+    public static Bitmap getFitSampleBitmap(InputStream inputStream) throws Exception {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        byte[] bytes = readStream(inputStream);
+        BitmapFactory.decodeByteArray(bytes, 0, bytes.length, options);
+        options.inSampleSize = 2;
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length, options);
+    }
+
+    /**
+     * 从inputStream中获取字节流 数组大小
+     **/
+    public static byte[] readStream(InputStream inStream) throws Exception {
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        while ((len = inStream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, len);
+        }
+        outStream.close();
+        inStream.close();
+        return outStream.toByteArray();
+    }
+}
---
+++ CustomGlide/app/src/main/java/com/cyq/customglide/resource/ValueCallback.java
@@ -0,0 +1,17 @@
+package com.cyq.customglide.resource;
+
+/**
+ * Time: 2019-09-21 15:04
+ * Author: ChenYangQi
+ * Description:
+ */
+public interface ValueCallback {
+
+    /**
+     * 监听的方法(Value不再使用了)
+     *
+     * @param key
+     * @param value
+     */
+    public void valueNonUseListener(String key, Value value);
+}
