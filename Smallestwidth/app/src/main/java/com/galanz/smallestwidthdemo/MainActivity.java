39a0f6240e7ab1bc3517bcf5cc411722c4f96647
chenyq003 <chenyq003@galanz.com>
2019-12-04 15:29:29 +0800

    feat:新增smallestwidth demo

---
+++ Smallestwidth/app/src/main/java/com/galanz/smallestwidthdemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.galanz.smallestwidthdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author ChenYangQi
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
