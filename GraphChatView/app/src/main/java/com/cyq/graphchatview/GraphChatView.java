618ef0b2bf7535696575d3bd4e51e230c4c8e717
chenyq003 <chenyq003@galanz.com>
2019-12-12 10:47:18 +0800

    feat:曲线完成

--- GraphChatView/app/src/main/java/com/cyq/graphchatview/GraphChatView.java
+++ GraphChatView/app/src/main/java/com/cyq/graphchatview/GraphChatView.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathMeasure;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -71,8 +74,9 @@ public class GraphChatView extends View {
 
         graphPaint = new Paint();
         graphPaint.setColor(graphColor);
+        graphPaint.setAntiAlias(true);
         graphPaint.setStrokeWidth(graphWidth);
-        graphPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        graphPaint.setStyle(Paint.Style.STROKE);
 
         yRoller.add("0");
         yRoller.add("50");
@@ -83,22 +87,25 @@ public class GraphChatView extends View {
 
         TempBean tempBean1 = new TempBean();
         tempBean1.setTimestamp(1575993600);
-        tempBean1.setTemp(0);
+        tempBean1.setTemp(20);
         TempBean tempBean2 = new TempBean();
         tempBean2.setTimestamp(1575994500); //15
-        tempBean2.setTemp(50);
+        tempBean2.setTemp(200);
         TempBean tempBean3 = new TempBean();
         tempBean3.setTimestamp(1575994800); //20
-        tempBean3.setTemp(100);
+        tempBean3.setTemp(210);
         TempBean tempBean4 = new TempBean();
         tempBean4.setTimestamp(1575994920); //22
-        tempBean4.setTemp(150);
+        tempBean4.setTemp(218);
         TempBean tempBean5 = new TempBean();
         tempBean5.setTimestamp(1575995400);//30
-        tempBean5.setTemp(200);
+        tempBean5.setTemp(260);
         TempBean tempBean6 = new TempBean();
         tempBean6.setTimestamp(1575995700);//35
-        tempBean6.setTemp(250);
+        tempBean6.setTemp(260);
+        TempBean tempBean7 = new TempBean();
+        tempBean7.setTimestamp(1575995880);//38
+        tempBean7.setTemp(260);
 
         tempList.add(tempBean1);
         tempList.add(tempBean2);
@@ -174,14 +181,121 @@ public class GraphChatView extends View {
         float xTotleTime = infoList.getMaxMillis() * 60;
         float scale = timeDifference / xTotleTime;
 
+        mPointList.clear();
         for (int i = 0; i < tempList.size(); i++) {
             int temp = tempList.get(i).getTemp();
             float timestamp = tempList.get(i).getTimestamp();
             int y = temp * (originY + yTvHeight / 2) / 300;
             int x = (int) ((timestamp - timestampStart) * (getWidth() - originX) * scale / (timeStampEnd - timestampStart));
-            canvas.drawPoint(x, -y, graphPaint);
+            Point point = new Point(x, -y);
+            mPointList.add(point);
         }
-        canvas.restore();
+        measurePath();
+        Path dst = new Path();
+        dst.rLineTo(0, 0);
+        float distance = mPathMeasure.getLength() * drawScale;
+        if (mPathMeasure.getSegment(0, distance, dst, true)) {
+            //绘制线
+            canvas.drawPath(dst, graphPaint);
+        }
+    }
+
+    private float lineSmoothness = 0.2f;
+    private List<Point> mPointList = new ArrayList<>();
+    private Path mPath;
+    private Path mAssistPath;
+    private float drawScale = 1f;
+    private PathMeasure mPathMeasure;
+
+    private void measurePath() {
+        mPath = new Path();
+        mAssistPath = new Path();
+        float prePreviousPointX = Float.NaN;
+        float prePreviousPointY = Float.NaN;
+        float previousPointX = Float.NaN;
+        float previousPointY = Float.NaN;
+        float currentPointX = Float.NaN;
+        float currentPointY = Float.NaN;
+        float nextPointX;
+        float nextPointY;
+
+        final int lineSize = mPointList.size();
+        for (int valueIndex = 0; valueIndex < lineSize; ++valueIndex) {
+            if (Float.isNaN(currentPointX)) {
+                Point point = mPointList.get(valueIndex);
+                currentPointX = point.x;
+                currentPointY = point.y;
+            }
+            if (Float.isNaN(previousPointX)) {
+                //是否是第一个点
+                if (valueIndex > 0) {
+                    Point point = mPointList.get(valueIndex - 1);
+                    previousPointX = point.x;
+                    previousPointY = point.y;
+                } else {
+                    //是的话就用当前点表示上一个点
+                    previousPointX = currentPointX;
+                    previousPointY = currentPointY;
+                }
+            }
+
+            if (Float.isNaN(prePreviousPointX)) {
+                //是否是前两个点
+                if (valueIndex > 1) {
+                    Point point = mPointList.get(valueIndex - 2);
+                    prePreviousPointX = point.x;
+                    prePreviousPointY = point.y;
+                } else {
+                    //是的话就用当前点表示上上个点
+                    prePreviousPointX = previousPointX;
+                    prePreviousPointY = previousPointY;
+                }
+            }
+
+            // 判断是不是最后一个点了
+            if (valueIndex < lineSize - 1) {
+                Point point = mPointList.get(valueIndex + 1);
+                nextPointX = point.x;
+                nextPointY = point.y;
+            } else {
+                //是的话就用当前点表示下一个点
+                nextPointX = currentPointX;
+                nextPointY = currentPointY;
+            }
+
+            if (valueIndex == 0) {
+                // 将Path移动到开始点
+                mPath.moveTo(currentPointX, currentPointY);
+                mAssistPath.moveTo(currentPointX, currentPointY);
+            } else {
+                // 求出控制点坐标
+                final float firstDiffX = (currentPointX - prePreviousPointX);
+                final float firstDiffY = (currentPointY - prePreviousPointY);
+                final float secondDiffX = (nextPointX - previousPointX);
+                final float secondDiffY = (nextPointY - previousPointY);
+                final float firstControlPointX = previousPointX + (lineSmoothness * firstDiffX);
+                final float firstControlPointY = previousPointY + (lineSmoothness * firstDiffY);
+                final float secondControlPointX = currentPointX - (lineSmoothness * secondDiffX);
+                final float secondControlPointY = currentPointY - (lineSmoothness * secondDiffY);
+                //画出曲线
+                mPath.cubicTo(firstControlPointX, firstControlPointY, secondControlPointX,
+                        secondControlPointY,
+                        currentPointX, currentPointY);
+                //将控制点保存到辅助路径上
+                mAssistPath.lineTo(firstControlPointX, firstControlPointY);
+                mAssistPath.lineTo(secondControlPointX, secondControlPointY);
+                mAssistPath.lineTo(currentPointX, currentPointY);
+            }
+
+            // 更新值,
+            prePreviousPointX = previousPointX;
+            prePreviousPointY = previousPointY;
+            previousPointX = currentPointX;
+            previousPointY = currentPointY;
+            currentPointX = nextPointX;
+            currentPointY = nextPointY;
+        }
+        mPathMeasure = new PathMeasure(mPath, false);
     }
 
     /**
