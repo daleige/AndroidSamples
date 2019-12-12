c145a3b609c1da321766e5350fa04cae8805cc85
chenyq003 <chenyq003@galanz.com>
2019-12-12 14:24:48 +0800

    feat:公开外部方法

--- GraphChatView/app/src/main/java/com/cyq/graphchatview/GraphChatView.java
+++ GraphChatView/app/src/main/java/com/cyq/graphchatview/GraphChatView.java
@@ -50,6 +50,7 @@ public class GraphChatView extends View {
     private List<TempBean> tempList = new ArrayList<>();
     private List<String> xRoller = new ArrayList<>();
     private List<String> yRoller = new ArrayList<>();
+    private int yScale = 50;
 
     public GraphChatView(Context context) {
         this(context, null);
@@ -102,47 +103,32 @@ public class GraphChatView extends View {
         yRoller.add("150");
         yRoller.add("200");
         yRoller.add("250");
+    }
 
-        TempBean tempBean1 = new TempBean();
-        tempBean1.setTimestamp(1575993600);
-        tempBean1.setTemp(20);
-        TempBean tempBean2 = new TempBean();
-        tempBean2.setTimestamp(1575994500); //15
-        tempBean2.setTemp(90);
-        TempBean tempBean3 = new TempBean();
-        tempBean3.setTimestamp(1575994800); //20
-        tempBean3.setTemp(170);
-        TempBean tempBean4 = new TempBean();
-        tempBean4.setTimestamp(1575994920); //22
-        tempBean4.setTemp(218);
-        TempBean tempBean5 = new TempBean();
-        tempBean5.setTimestamp(1575995400);//30
-        tempBean5.setTemp(260);
-        TempBean tempBean6 = new TempBean();
-        tempBean6.setTimestamp(1575995700);//35
-        tempBean6.setTemp(260);
-        TempBean tempBean7 = new TempBean();
-        tempBean7.setTimestamp(1575995880);//38
-        tempBean7.setTemp(260);
-        TempBean tempBean8 = new TempBean();
-        tempBean8.setTimestamp(1575996180);//43
-        tempBean8.setTemp(260);
-        tempList.add(tempBean1);
-        tempList.add(tempBean2);
-        tempList.add(tempBean3);
-        tempList.add(tempBean4);
-        tempList.add(tempBean5);
-        tempList.add(tempBean6);
-        tempList.add(tempBean7);
-        tempList.add(tempBean8);
+    /**
+     * 设置Y轴刻度
+     *
+     * @param scale 单位刻度
+     * @param strs  刻度上展示的文字，若不设置则展示0,50,100,150,200,250
+     */
+    public void setYAxis(int scale, List<String> strs) {
+        if (strs.size() > 0) {
+            yScale = scale;
+            yRoller.clear();
+            yRoller = strs;
+        }
+    }
+
+    public void setTempList(List<TempBean> tempList) {
+        if (tempList.size() > 0) {
+            this.tempList = tempList;
+            invalidate();
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-
-
         TimeUtils.XRollerInfo infoList = TimeUtils.getXRollerInfo(tempList);
         if (infoList != null) {
             xRoller.clear();
--- GraphChatView/app/src/main/java/com/cyq/graphchatview/MainActivity.java
+++ GraphChatView/app/src/main/java/com/cyq/graphchatview/MainActivity.java
@@ -4,11 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private GraphChatView mGraphChatView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<TempBean> tempList = new ArrayList<>();
+        TempBean tempBean1 = new TempBean();
+        tempBean1.setTimestamp(1575993600);
+        tempBean1.setTemp(20);
+        TempBean tempBean2 = new TempBean();
+        tempBean2.setTimestamp(1575994500); //15
+        tempBean2.setTemp(90);
+        TempBean tempBean3 = new TempBean();
+        tempBean3.setTimestamp(1575994800); //20
+        tempBean3.setTemp(170);
+        TempBean tempBean4 = new TempBean();
+        tempBean4.setTimestamp(1575994920); //22
+        tempBean4.setTemp(218);
+        TempBean tempBean5 = new TempBean();
+        tempBean5.setTimestamp(1575995400);//30
+        tempBean5.setTemp(260);
+        TempBean tempBean6 = new TempBean();
+        tempBean6.setTimestamp(1575995700);//35
+        tempBean6.setTemp(260);
+        TempBean tempBean7 = new TempBean();
+        tempBean7.setTimestamp(1575995880);//38
+        tempBean7.setTemp(260);
+        TempBean tempBean8 = new TempBean();
+        tempBean8.setTimestamp(1575996180);//43
+        tempBean8.setTemp(260);
+        tempList.add(tempBean1);
+        tempList.add(tempBean2);
+        tempList.add(tempBean3);
+        tempList.add(tempBean4);
+        tempList.add(tempBean5);
+        tempList.add(tempBean6);
+        tempList.add(tempBean7);
+        tempList.add(tempBean8);
+
+        mGraphChatView = findViewById(R.id.graph_view);
+        mGraphChatView.setTempList(tempList);
     }
 }
