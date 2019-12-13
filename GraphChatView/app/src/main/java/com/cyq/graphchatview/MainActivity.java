a0d34d68937c2439f71f3bede9e45b0ca2abf722
chenyq003 <chenyq003@galanz.com>
2019-12-13 08:57:29 +0800

    feat:最终完成2

--- GraphChatView/app/src/main/java/com/cyq/graphchatview/MainActivity.java
+++ GraphChatView/app/src/main/java/com/cyq/graphchatview/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cyq.graphchatview;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,27 +11,40 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private GraphChatView mGraphChatView;
+    private List<TempBean> tempList = new ArrayList<>();
+    private Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<TempBean> tempList = new ArrayList<>();
-        Random random = new Random();
-        int temp = 0;
-        for (int i = 0; i < 7; i++) {
+
+        random = new Random();
+        for (int i = 0; i < 9; i++) {
             TempBean tempBean = new TempBean();
             tempBean.setTimestamp(System.currentTimeMillis() / 1000 + 300 * i);
-            temp += (random.nextInt(50)+20);
-            if (temp < 300) {
-                tempBean.setTemp(temp);
-                tempList.add(tempBean);
-            }
+            tempBean.setTemp(random.nextInt(260));
+            tempList.add(tempBean);
         }
 
         mGraphChatView = findViewById(R.id.graph_view);
-//        mGraphChatView.setYAxis(60, "0", "60", "120", "180", "240", "300");
+        mGraphChatView.setTempList(tempList);
+
+        /**
+         * 设置Y轴坐标
+         */
+        //mGraphChatView.setYAxis(60, "0", "60", "120", "180", "240", "300");
+    }
+
+    public void refreshData(View view) {
+        tempList.clear();
+        for (int i = 0; i < 9; i++) {
+            TempBean tempBean = new TempBean();
+            tempBean.setTimestamp(System.currentTimeMillis() / 1000 + 300 * i);
+            tempBean.setTemp(random.nextInt(260));
+            tempList.add(tempBean);
+        }
         mGraphChatView.setTempList(tempList);
     }
 }
