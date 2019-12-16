bba60eb73282c3e4d9bc6b739c6f2768ffaed2ab
chenyq003 <chenyq003@galanz.com>
2019-12-16 09:41:49 +0800

    create WifiLibrary

--- WifiSetting/app/src/main/java/com/cyq/wifisetting/MainActivity.java
+++ WifiSetting/app/src/main/java/com/cyq/wifisetting/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * @author ChenYangQi
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
