b6fe51e4b947c26245a1e90b915c7af1dad5022d
chenyq003 <chenyq003@galanz.com>
2020-01-07 13:48:55 +0800

    feat:实现Bitmap复用，图片区域加载

--- CustomViewDemo/app/src/main/java/com/cyq/customview/MainActivity.java
+++ CustomViewDemo/app/src/main/java/com/cyq/customview/MainActivity.java
@@ -4,13 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.cyq.customview.view.NeBigView;
-
 import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
-    private NeBigView mBigIv;
+    private BigImageView mBigIv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mBigIv=findViewById(R.id.iv_big);
         try {
-            InputStream inputStream = this.getAssets().open("aaa.jpg");
+            InputStream inputStream = this.getAssets().open("bbb.jpg");
             mBigIv.setImage(inputStream);
         } catch (IOException e) {
             e.printStackTrace();
