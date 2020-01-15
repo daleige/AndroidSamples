9603904f4920dbfcdb0310356222321a1ceeb93d
chenyq003 <chenyq003@galanz.com>
2020-01-15 16:32:46 +0800

    feat:自定义9宫格图片控件

--- CustomViewDemo/app/src/main/java/com/cyq/customview/flowLayout/FlowLayoutActivity.java
+++ CustomViewDemo/app/src/main/java/com/cyq/customview/flowLayout/FlowLayoutActivity.java
@@ -1,16 +1,55 @@
 package com.cyq.customview.flowLayout;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.bumptech.glide.Glide;
 import com.cyq.customview.R;
 
 public class FlowLayoutActivity extends AppCompatActivity {
+
+    private ImageView mIv1;
+    private ImageView mIv2;
+    private ImageView mIv3;
+    private ImageView mIv4;
+    private ImageView mIv5;
+    private ImageView mIv6;
+    private ImageView mIv7;
+    private ImageView mIv8;
+    private ImageView mIv9;
+
+    private final String URL_IMG = "http://q3x62hkt1.bkt.clouddn.com/banner/58f57dfa5bb73.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flow_layout);
+        initView();
     }
+
+    private void initView() {
+        mIv1 = (ImageView) findViewById(R.id.iv_1);
+        mIv2 = (ImageView) findViewById(R.id.iv_2);
+        mIv3 = (ImageView) findViewById(R.id.iv_3);
+        mIv4 = (ImageView) findViewById(R.id.iv_4);
+        mIv5 = (ImageView) findViewById(R.id.iv_5);
+        mIv6 = (ImageView) findViewById(R.id.iv_6);
+        mIv7 = (ImageView) findViewById(R.id.iv_7);
+        mIv8 = (ImageView) findViewById(R.id.iv_8);
+        mIv9 = (ImageView) findViewById(R.id.iv_9);
+
+        Glide.with(this).load(URL_IMG).into(mIv1);
+        Glide.with(this).load(URL_IMG).into(mIv2);
+        Glide.with(this).load(URL_IMG).into(mIv3);
+        Glide.with(this).load(URL_IMG).into(mIv4);
+        Glide.with(this).load(URL_IMG).into(mIv5);
+        Glide.with(this).load(URL_IMG).into(mIv6);
+        Glide.with(this).load(URL_IMG).into(mIv7);
+        Glide.with(this).load(URL_IMG).into(mIv8);
+        Glide.with(this).load(URL_IMG).into(mIv9);
+    }
+
+
 }
---
+++ CustomViewDemo/app/src/main/java/com/cyq/customview/flowLayout/NineImageLayout.java
@@ -0,0 +1,139 @@
+package com.cyq.customview.flowLayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author : ChenYangQi
+ * date   : 2020/1/15 14:59
+ * desc   : 九宫格图片控件
+ */
+public class NineImageLayout extends ViewGroup {
+    /**
+     * 控件宽度
+     */
+    private int width = 800;
+    /**
+     * 图片之间间隔的大小
+     */
+    private int itemMargin = 15;
+    /**
+     * 单个图片的宽度和高度
+     */
+    private int itemWidth;
+
+    public NineImageLayout(Context context) {
+        this(context, null);
+    }
+
+    public NineImageLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public NineImageLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        //一张图片的宽高
+        itemWidth = (width - 2 * itemMargin) / 3;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int viewHeight = 0;
+        int viewWidth = 0;
+        int count = getChildCount();
+        if (count == 1) {
+            //TODO 单独处理
+
+        } else if (count <= 3) {
+            viewHeight = itemWidth;
+            if (count == 2) {
+                viewWidth = 2 * itemWidth + itemMargin;
+            } else if (count == 3) {
+                viewWidth = width;
+            }
+        } else if (count <= 6) {
+            viewHeight = 2 * itemWidth + itemMargin;
+            if (count == 4) {
+                viewWidth = 2 * itemWidth + itemMargin;
+            } else {
+                viewWidth = width;
+            }
+        } else if (count <= 9) {
+            viewHeight = width;
+            viewWidth = width;
+        }
+        Log.i("test", "最终测量结果viewWidth：" + viewWidth + "  viewHeight:" + viewHeight);
+        setMeasuredDimension(viewWidth, viewHeight);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int count = getChildCount();
+        int top = 0;
+        int left = 0;
+        int right = 0;
+        int bottom = 0;
+
+        for (int i = 0; i < count; i++) {
+            View childView = getChildAt(i);
+            if (i == 0) {
+                if (count == 1) {
+                    //TODO 单独处理
+
+                } else {
+                    left = 0;
+                    top = 0;
+                    right = left + itemWidth;
+                    bottom = top + itemWidth;
+                }
+            } else if (i == 1) {
+                left = itemWidth + itemMargin;
+                top = 0;
+                right = left + itemWidth;
+                bottom = top + itemWidth;
+            } else if (i == 2) {
+                left = itemWidth * 2 + itemMargin * 2;
+                top = 0;
+                right = width;
+                bottom = top + itemWidth;
+            } else if (i == 3) {
+                left = 0;
+                top = itemWidth + itemMargin;
+                right = left + itemWidth;
+                bottom = top + itemWidth;
+            } else if (i == 4) {
+                left = itemWidth + itemMargin;
+                top = itemWidth + itemMargin;
+                right = left + itemWidth;
+                bottom = top + itemWidth;
+            } else if (i == 5) {
+                left = (itemWidth + itemMargin) * 2;
+                top = itemWidth + itemMargin;
+                right = width;
+                bottom = top + itemWidth;
+            } else if (i == 6) {
+                left = 0;
+                top = (itemWidth + itemMargin) * 2;
+                right = left + itemWidth;
+                bottom = width;
+            } else if (i == 7) {
+                left = itemWidth + itemMargin;
+                top = (itemWidth + itemMargin) * 2;
+                right = left + itemWidth;
+                bottom = width;
+            } else if (i == 8) {
+                left = (itemWidth + itemMargin) * 2;
+                top = (itemWidth + itemMargin) * 2;
+                right = width;
+                bottom = width;
+            }
+            childView.layout(left, top, right, bottom);
+        }
+    }
+
+
+}
