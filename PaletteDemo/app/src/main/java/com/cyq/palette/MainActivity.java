1686b63ed46c5c87bd6ab0d533ef8f5d56b681bd
chenyq003 <chenyq003@galanz.com>
2020-01-19 17:52:30 +0800

    feat:边界颜色处理

--- PaletteDemo/app/src/main/java/com/cyq/palette/MainActivity.java
+++ PaletteDemo/app/src/main/java/com/cyq/palette/MainActivity.java
@@ -1,7 +1,11 @@
 package com.cyq.palette;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
@@ -9,7 +13,10 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.palette.graphics.Palette;
+import androidx.palette.graphics.Target;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
@@ -19,11 +26,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private int COLOR_DEFAULT = Color.WHITE;
     private ViewPager mViewPager;
     private FrameLayout mFlContainer;
     private List<Integer> mDate = new ArrayList<>();
     private List<ImageView> views = new ArrayList<>();
     private PagerAdapter mAdapter;
+    private List<Integer> colors = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,13 @@ public class MainActivity extends AppCompatActivity {
         mDate.add(R.drawable.pig222);
         mDate.add(R.drawable.pig333);
         mDate.add(R.drawable.pig444);
+        mDate.add(R.drawable.pig555);
+        mDate.add(R.drawable.pig666);
+        mDate.add(R.drawable.pig777);
+        mDate.add(R.drawable.pig888);
+        mDate.add(R.drawable.pig999);
+        mDate.add(R.drawable.pig10);
+
         for (int resourceId : mDate) {
             ImageView imageView = new ImageView(this);
             imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
@@ -48,7 +64,41 @@ public class MainActivity extends AppCompatActivity {
         mFlContainer = findViewById(R.id.fl_container);
         mAdapter = new MyPageAdapter(this, views);
         mViewPager.setAdapter(mAdapter);
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.i("test", "position:" + position);
+                fetchColor(mDate.get(position));
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
-
+    /**
+     * 计算颜色
+     *
+     * @param resourceId
+     */
+    private void fetchColor(int resourceId) {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), resourceId);
+        Palette.Builder paletteBuild = Palette.from(bitmap);
+        paletteBuild.generate(new Palette.PaletteAsyncListener() {
+            @Override
+            public void onGenerated(@Nullable Palette palette) {
+                Palette.Swatch dominantSwatch = palette.getSwatchForTarget(Target.VIBRANT);
+                int color = dominantSwatch != null ? dominantSwatch.getRgb() : COLOR_DEFAULT;
+                Log.i("test", "颜色：" + color);
+                mFlContainer.setBackgroundColor(color);
+            }
+        });
+    }
 }
