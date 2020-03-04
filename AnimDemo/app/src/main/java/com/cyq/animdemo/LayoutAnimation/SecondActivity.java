866a8babd897e74c784e91a668824449b7bca277
chenyq003 <chenyq003@galanz.com>
2020-03-04 17:08:43 +0800

    create:Create AnimDemo Project

---
+++ AnimDemo/app/src/main/java/com/cyq/animdemo/LayoutAnimation/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.cyq.animdemo.LayoutAnimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cyq.animdemo.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+
+}
