e7ea5f25d5579a6a360e3820e37094156a63c426
chenyq003 <chenyq003@galanz.com>
2020-03-04 17:43:39 +0800

    feat:layoutAnimation实现错帧动画

--- AnimDemo/app/src/main/java/com/cyq/animdemo/MainActivity.java
+++ AnimDemo/app/src/main/java/com/cyq/animdemo/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.LinearLayout;
 
 import com.cyq.animdemo.LayoutAnimation.SecondActivity;
 
@@ -14,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout linearLayout = findViewById(R.id.ll_container);
+        LayoutAnimationController controller = AnimationUtils.loadLayoutAnimation(this, R.anim.left_to_right_layout_animation);
+        linearLayout.setLayoutAnimation(controller);
     }
 
     public void next(View view) {
