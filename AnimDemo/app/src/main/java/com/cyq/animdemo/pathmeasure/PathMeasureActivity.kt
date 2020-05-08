9a6473254ebf1a134f307cfb8cd410e7194b6e65
陈扬齐 <chenyq003@galanz.com>
2020-05-08 18:21:41 +0800

    feat:PathMeasure练习1

---
+++ AnimDemo/app/src/main/java/com/cyq/animdemo/pathmeasure/PathMeasureActivity.kt
@@ -0,0 +1,12 @@
+package com.cyq.animdemo.pathmeasure
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cyq.animdemo.R
+
+class PathMeasureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_path_measure)
+    }
+}
---
+++ AnimDemo/app/src/main/java/com/cyq/animdemo/pathmeasure/PathMeasureView.kt
@@ -0,0 +1,22 @@
+package com.cyq.animdemo.pathmeasure
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ *    @author : ChenYangQi
+ *    date   : 2020/5/8 18:20
+ *    desc   :
+ */
+class PathMeasureView : View {
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+            context,
+            attrs,
+            defStyleAttr
+    )
+}
