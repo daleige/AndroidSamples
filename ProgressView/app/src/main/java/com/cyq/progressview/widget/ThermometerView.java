9a780c84ce15a050e4f4b3ee08da85c45f4430f1
陈扬齐 <chenyq003@galanz.com>
2020-05-14 15:47:01 +0800

    feat:扇形裁剪

--- ProgressView/app/src/main/java/com/cyq/progressview/widget/ThermometerView.java
+++ ProgressView/app/src/main/java/com/cyq/progressview/widget/ThermometerView.java
@@ -6,8 +6,13 @@ import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.RadialGradient;
+import android.graphics.RectF;
 import android.graphics.Shader;
+import android.graphics.Xfermode;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -26,27 +31,48 @@ import java.util.Random;
  */
 public class ThermometerView extends View {
     private Context mContext;
-    //控件宽高
+    /**
+     * 控件宽高
+     */
     private int width, height;
-    //粒子圆环的宽度
+    /**
+     * 粒子圆环的宽度
+     */
     private final int mCircleWidth = 30;
-    //粒子总个数
+    /**
+     * 粒子总个数
+     */
     private int pointCount = 100;
-    //粒子列表
+    /**
+     * 粒子列表
+     */
     private List<AnimPoint> mPointList = new ArrayList<>(pointCount);
-    //粒子外层圆环原点坐标和半径长度
+    /**
+     * 粒子外层圆环原点坐标和半径长度
+     */
     private int centerX, centerY, radius;
-    //粒子外层圆环的画笔
+    /**
+     * 粒子外层圆环的画笔
+     */
     private Paint mCirclePaint;
-    //粒子画笔
+    /**
+     * 粒子画笔
+     */
     private Paint mPointPaint;
-    private Paint mLinePaint;
-    //白色
+    /**
+     * 圆弧画笔
+     */
+    private Paint mArcPaint;
+
+    private Paint mArcPathPaint;
+
+    /**
+     * 白色
+     */
     private int whiteColor = Color.parseColor("#FFFFFFFF");
     private int blackColor = Color.parseColor("#FF000000");
     private int redColor = Color.parseColor("#f44336");
     private int yellowColor = Color.parseColor("#4caf50");
-
 
     private int beginRadialGradientColor = Color.parseColor("#66001BFF");
     private int endRadialGradientColor = Color.parseColor("#1978FF");
@@ -92,12 +118,6 @@ public class ThermometerView extends View {
         mPointPaint.setStyle(Paint.Style.FILL);
         mPointPaint.setMaskFilter(new BlurMaskFilter(5, BlurMaskFilter.Blur.NORMAL));
 
-        mLinePaint = new Paint();
-        mLinePaint.setColor(yellowColor);
-        mLinePaint.setAntiAlias(true);
-        mLinePaint.setStrokeWidth(1);
-        mLinePaint.setStyle(Paint.Style.FILL);
-
         mSweptPaint = new Paint();
         mSweptPaint.setStyle(Paint.Style.FILL_AND_STROKE);
         mSweptPaint.setColor(radialCircleColor);
@@ -110,7 +130,22 @@ public class ThermometerView extends View {
                 Shader.TileMode.CLAMP);
         mSweptPaint.setShader(mRadialGradient);
 
+        mArcPaint = new Paint();
+        mArcPaint.setColor(Color.RED);
+        mArcPaint.setStyle(Paint.Style.FILL);
+        mArcPaint.setStrokeWidth(20);
 
+        mArcPathPaint = new Paint();
+        mArcPathPaint.setStrokeWidth(4);
+        mArcPathPaint.setColor(Color.RED);
+        mArcPathPaint.setStyle(Paint.Style.STROKE);
+        mArcPathPaint.setAntiAlias(true);
+
+        mXfermode = new PorterDuffXfermode(PorterDuff.Mode.DST_ATOP);
+
+        mRect = new RectF(-centerX, -centerY, centerX, centerY);
+        mArcPath = new Path();
+        mArcPath = getSectorClip(centerX, centerY, radius, 0, 150);
         mPointList.clear();
         AnimPoint animPoint = new AnimPoint();
         animPoint.setAlpha(1);
@@ -146,25 +181,54 @@ public class ThermometerView extends View {
         setMeasuredDimension(width, height);
     }
 
+    private Xfermode mXfermode;
+    private Path mArcPath;
+    private RectF mRect;
+
     @Override
     protected void onDraw(final Canvas canvas) {
         super.onDraw(canvas);
-
-        //画测试坐标线
-//        canvas.drawLine(radius, 0, radius, height, mLinePaint);
-//        canvas.drawLine(0, radius, width, radius, mLinePaint);
+        canvas.translate(centerX, centerY);
 
         //画动画粒子
-        canvas.translate(centerX, centerY);
         for (AnimPoint animPoint : mPointList) {
             canvas.drawCircle(animPoint.getmX(), animPoint.getmY(),
                     animPoint.getRadius(), mPointPaint);
         }
-
         //画渐变进度圆
         canvas.drawCircle(0, 0, radius, mSweptPaint);
-
         //渐变进度圆的外层圆环
         canvas.drawCircle(0, 0, radius, mCirclePaint);
+
+        //设置图层混合模式
+        //  mArcPaint.setXfermode(mXfermode);
+//        //画圆弧的角度
+//        int layoutId = canvas.saveLayer(0, 0, width, height, mArcPaint, Canvas.ALL_SAVE_FLAG);
+//        canvas.drawArc(0, 0, radius * 2, radius * 2, 0, 120, true, mArcPaint);
+//        canvas.restoreToCount(layoutId);
+//        canvas.restore();
+
+        canvas.drawPath(mArcPath, mArcPathPaint);
+    }
+
+    private Path getSectorClip(float center_X, float center_Y, float r, float startAngle, float sweepAngle) {
+        Path path = new Path();
+        //下面是获得一个三角形的剪裁区
+        //圆心
+        //起始点角度在圆上对应的横坐标
+        path.moveTo(center_X, center_Y);
+        //起始点角度在圆上对应的纵坐标
+        path.lineTo((float) (center_X + r * Math.cos(startAngle * Math.PI / 180)),
+                (float) (center_Y + r * Math.sin(startAngle * Math.PI / 180)));
+        //终点角度在圆上对应的横坐标
+        path.lineTo((float) (center_X + r * Math.cos(sweepAngle * Math.PI / 180)),
+                (float) (center_Y + r * Math.sin(sweepAngle * Math.PI / 180)));
+        //终点点角度在圆上对应的纵坐标
+        path.close();
+        //设置一个正方形，内切圆
+        RectF rectF = new RectF(center_X - r, center_Y - r, center_X + r, center_Y + r);
+        //下面是获得弧形剪裁区的方法
+        path.addArc(rectF, startAngle, sweepAngle - startAngle);
+        return path;
     }
 }
