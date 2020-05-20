51ab405e9c5fd623fd5940209ca081f33368a5e0
陈扬齐 <chenyq003@galanz.com>
2020-05-20 17:55:11 +0800

    feat:扇形裁剪

---
+++ ProgressView/app/src/main/java/com/cyq/progressview/evaluator/MyColors.java
@@ -0,0 +1,47 @@
+package com.cyq.progressview.evaluator;
+
+/**
+ * @author : ChenYangQi
+ * date   : 2020/5/20 17:43
+ * desc   : 自定义动画属性，适用于进度圆进度变化时几种颜色的变化规律
+ */
+public class MyColors {
+    /**
+     * 外圈颜色
+     */
+    private int outColor;
+
+    /**
+     * 渐变色开始颜色，渐变圈外圈颜色
+     */
+    private int beginColor;
+
+    /**
+     * 渐变色结束颜色，渐变圈内圈颜色
+     */
+    private int endColor;
+
+    public int getOutColor() {
+        return outColor;
+    }
+
+    public void setOutColor(int outColor) {
+        this.outColor = outColor;
+    }
+
+    public int getBeginColor() {
+        return beginColor;
+    }
+
+    public void setBeginColor(int beginColor) {
+        this.beginColor = beginColor;
+    }
+
+    public int getEndColor() {
+        return endColor;
+    }
+
+    public void setEndColor(int endColor) {
+        this.endColor = endColor;
+    }
+}
---
+++ ProgressView/app/src/main/java/com/cyq/progressview/evaluator/MyColorsEvaluator.java
@@ -0,0 +1,18 @@
+package com.cyq.progressview.evaluator;
+
+import android.animation.TypeEvaluator;
+
+/**
+ * @author : ChenYangQi
+ * date   : 2020/5/20 17:42
+ * desc   :
+ */
+public class MyColorsEvaluator implements TypeEvaluator<MyColors> {
+
+    @Override
+    public MyColors evaluate(float fraction, MyColors startValue, MyColors endValue) {
+
+
+        return null;
+    }
+}
