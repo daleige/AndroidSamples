09a464f5756292813625053abc6590bd2e7469d3
陈扬齐 <chenyq003@galanz.com>
2020-07-27 17:51:47 +0800

    feat：调整数字的大小

---
+++ ProgressView/app/src/main/java/com/cyq/progressview/test/TestActivity.java
@@ -0,0 +1,16 @@
+package com.cyq.progressview.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cyq.progressview.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
