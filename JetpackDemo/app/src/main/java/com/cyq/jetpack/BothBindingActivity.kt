46750187c16b3167c09d87ea25ab15c5d50ef48c
陈扬齐 <chenyq003@galanz.com>
2020-10-10 18:01:15 +0800

    feat:自定义DataBinding加载图片

---
+++ JetpackDemo/app/src/main/java/com/cyq/jetpack/BothBindingActivity.kt
@@ -0,0 +1,15 @@
+package com.cyq.jetpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * 双向绑定
+ */
+class BothBindingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_both_binding)
+
+    }
+}
--- JetpackDemo/app/src/main/java/com/cyq/jetpack/event/EventHandleListener.kt
+++ JetpackDemo/app/src/main/java/com/cyq/jetpack/event/EventHandleListener.kt
@@ -1,6 +1,8 @@
 package com.cyq.jetpack.event
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -14,4 +16,9 @@ class EventHandleListener(private var context: Context) {
     fun onButtonClicked(view: View) {
         Toast.makeText(context, "click:" + (view as Button).text, Toast.LENGTH_SHORT).show()
     }
+
+    fun openActivity(view: View, activity: Class<Activity>) {
+        context.startActivity(Intent(context, activity))
+    }
 }
+
