b9025f3f93a10095ac75e13d75088c117c84a942
陈扬齐 <chenyq003@galanz.com>
2020-10-10 14:35:51 +0800

    feat:布局绑定对象

--- JetpackDemo/app/src/main/java/com/cyq/jetpack/DataBindingActivity.kt
+++ JetpackDemo/app/src/main/java/com/cyq/jetpack/DataBindingActivity.kt
@@ -2,11 +2,22 @@ package com.cyq.jetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.cyq.jetpack.bean.Book
+import com.cyq.jetpack.databinding.ActivityDataBindingBinding
 
 class DataBindingActivity : AppCompatActivity() {
+    val book: Book by lazy {
+        val book = Book("张三", 5, "设计模式之禅")
+        book
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_data_binding)
+        val activityDataBinding: ActivityDataBindingBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_data_binding)
+        //activityDataBinding.setVariable(BR.book, book)
+        activityDataBinding.book = book
 
     }
 }
---
+++ JetpackDemo/app/src/main/java/com/cyq/jetpack/bean/Book.kt
@@ -0,0 +1,12 @@
+package com.cyq.jetpack.bean
+
+/**
+ *    @author : ChenYangQi
+ *    date   : 2020/10/10 14:00
+ *    desc   :
+ */
+data class Book(
+    val author: String,
+    val rating: Int,
+    val title: String
+)
