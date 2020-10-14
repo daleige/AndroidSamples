2e250222e58ea948bb6ae591b7e31979c88b2374
陈扬齐 <chenyq003@galanz.com>
2020-10-14 10:24:50 +0800

    feat：移除library module

---
+++ ModuleDemo/order/src/main/java/com/cyq/order/Order_MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cyq.order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Order_MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order__main)
+    }
+}
---
+++ ModuleDemo/personal/src/main/java/com/cyq/personal/Personal_MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cyq.personal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Personal_MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.personal_activity_main)
+    }
+}
