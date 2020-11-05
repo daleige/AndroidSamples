f082b00110edf648dd28b05b3774ab902df37bb1
陈扬齐 <chenyq003@galanz.com>
2020-11-05 18:03:08 +0800

    fix：线程间交互

--- DesignPatterns/src/com/cyq/design/多线程/Test.java
+++ DesignPatterns/src/com/cyq/design/多线程/Test.java
@@ -4,29 +4,21 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class Test {
+    //https://www.cnblogs.com/onlywujun/p/3565082.html
     public static void main(String[] args) throws InterruptedException {
 
         Thread thread1=new Thread(new Runnable() {
             @Override
             public void run() {
-
-            }
-        });
-
-        Thread thread2=new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(10*1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                System.out.println(Thread.currentThread().getName()+"线程 start");
+                while (!Thread.currentThread().isInterrupted()){
+                    System.out.println(Thread.currentThread().getName()+"线程被中断");
                 }
+                System.out.println(Thread.currentThread().getName()+"线程 end");
             }
         });
 
         thread1.start();
-        thread2.start();
-
         Thread.sleep(2000);
         thread1.interrupt();
     }
