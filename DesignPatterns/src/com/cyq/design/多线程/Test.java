2c26cbaf2a9448328a17ac0663e7d8fdec00a167
陈扬齐 <chenyq003@galanz.com>
2020-11-06 10:22:27 +0800

    fix：提交Thread.interrupt测试

--- DesignPatterns/src/com/cyq/design/多线程/Test.java
+++ DesignPatterns/src/com/cyq/design/多线程/Test.java
@@ -1,25 +1,28 @@
 package com.cyq.design.多线程;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-
 public class Test {
     //https://www.cnblogs.com/onlywujun/p/3565082.html
     public static void main(String[] args) throws InterruptedException {
 
-        Thread thread1=new Thread(new Runnable() {
+        Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println(Thread.currentThread().getName()+"线程 start");
-                while (!Thread.currentThread().isInterrupted()){
-                    System.out.println(Thread.currentThread().getName()+"线程被中断");
+                System.out.println(Thread.currentThread().getName() + "线程 start");
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        Thread.sleep(900);
+                        System.out.println(Thread.currentThread().getName() + "执行 io 操作....");
+                    } catch (InterruptedException e) {
+                        System.out.println(Thread.currentThread().getName() + "收到中断命令了---");
+                        return;
+                    }
                 }
-                System.out.println(Thread.currentThread().getName()+"线程 end");
+                System.out.println(Thread.currentThread().getName() + "线程 end");
             }
         });
 
         thread1.start();
-        Thread.sleep(2000);
+        Thread.sleep(3000);
         thread1.interrupt();
     }
 }
