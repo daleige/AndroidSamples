543064941681855330d0c1ac26f39a852823f550
陈扬齐 <chenyq003@galanz.com>
2020-11-16 15:58:31 +0800

    feat:navigation实现fragment导航

--- JetpackDemo/app/src/main/java/com/cyq/jetpack/navigation/MainFragment.kt
+++ JetpackDemo/app/src/main/java/com/cyq/jetpack/navigation/MainFragment.kt
@@ -7,50 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import com.cyq.jetpack.R
-import kotlinx.android.synthetic.main.activity_data_binding.*
+import kotlinx.android.synthetic.main.fragment_main.view.*
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-class MainFragment : Fragment(), View.OnClickListener {
-    private var param1: String? = null
-    private var param2: String? = null
+class MainFragment : Fragment() {
     private lateinit var rootView: View
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         rootView = inflater.inflate(R.layout.fragment_main, container, false)
-        btnClick.setOnClickListener(this)
-        return rootView
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.btnClick -> {
-                Navigation.findNavController(v)
-                    .navigate(R.id.action_mainFragment_to_secondFragment)
-            }
+        rootView.btnSkip.setOnClickListener {
+            val navController = Navigation.findNavController(it)
+            navController.navigate(R.id.action_mainFragment_to_secondFragment)
         }
+        return rootView
     }
 }
---
+++ JetpackDemo/app/src/main/java/com/cyq/jetpack/navigation/NavigationActivity.kt
@@ -0,0 +1,12 @@
+package com.cyq.jetpack.navigation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cyq.jetpack.R
+
+class NavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+    }
+}
