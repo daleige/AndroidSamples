b2af4e64b15905eb34bfa6a42c7b2898f5246ebd
陈扬齐 <766790183@qq.com>
2021-07-19 18:57:11 +0800

    feat:android嵌套滚动demo

---
+++ NestedScroll/app/src/main/java/com/chenyangqi/nested/scroll/nesting/MyNestedScrollPreView.kt
@@ -0,0 +1,85 @@
+package com.chenyangqi.nested.scroll.nesting
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.LinearLayout
+import androidx.core.view.NestedScrollingParent3
+import androidx.core.view.ViewCompat
+
+/**
+ * @describe
+ * @author chenyangqi
+ * @time 2021/7/19 18:06
+ */
+class MyNestedScrollPreView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr), NestedScrollingParent3 {
+
+    private var mHeaderView: View? = null
+    private var mHeaderHeight = 0
+
+    override fun onStartNestedScroll(child: View, target: View, axes: Int, type: Int): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL
+    }
+
+    override fun onNestedScrollAccepted(child: View, target: View, axes: Int, type: Int) {
+
+    }
+
+    override fun onStopNestedScroll(target: View, type: Int) {
+
+    }
+
+    override fun onNestedScroll(
+        target: View,
+        dxConsumed: Int,
+        dyConsumed: Int,
+        dxUnconsumed: Int,
+        dyUnconsumed: Int,
+        type: Int,
+        consumed: IntArray
+    ) {
+
+    }
+
+    override fun onNestedScroll(
+        target: View,
+        dxConsumed: Int,
+        dyConsumed: Int,
+        dxUnconsumed: Int,
+        dyUnconsumed: Int,
+        type: Int
+    ) {
+
+    }
+
+    override fun onNestedPreScroll(target: View, dx: Int, dy: Int, consumed: IntArray, type: Int) {
+        val headerScrollUp = dy > 0 && scrollY < mHeaderHeight
+        val headerScrollDown = dy < 0 && scrollY > 0 && !target.canScrollVertically(-1)
+        if (headerScrollUp || headerScrollDown) {
+            scrollBy(0, dy)
+            consumed[1] = dy
+        }
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        if (childCount > 0) {
+            //第一个子控件是HeaderView
+            mHeaderView = getChildAt(0)
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mHeaderHeight = mHeaderView?.measuredHeight ?: 0
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val totalHeight =
+            MeasureSpec.getSize(heightMeasureSpec + (mHeaderView?.measuredHeight ?: 0))
+        val model = MeasureSpec.getMode(heightMeasureSpec)
+        super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(totalHeight, model))
+    }
+}
--- NestedScroll/app/src/main/java/com/chenyangqi/nested/scroll/nesting/NestedActivity.kt
+++ NestedScroll/app/src/main/java/com/chenyangqi/nested/scroll/nesting/NestedActivity.kt
@@ -1,37 +1,34 @@
-package com.chenyangqi.nested.scroll
+package com.chenyangqi.nested.scroll.nesting
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.chenyangqi.nested.scroll.R
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var mRecyclerView: RecyclerView
-    private lateinit var mAdapter: MyAdapter
-
+class NestedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_nested)
 
+        val mRecyclerView = findViewById<RecyclerView>(R.id.mRecyclerView)
         val mData: MutableList<String> = ArrayList()
         for (i in 1..Int.MAX_VALUE / 2) {
             mData.add("item = $i")
         }
-
-        mRecyclerView = findViewById(R.id.mRecyclerView)
-        mAdapter = MyAdapter(mData)
-        mRecyclerView.adapter = mAdapter
+        val adapter = MyAdapter(mData)
         mRecyclerView.layoutManager = LinearLayoutManager(this)
+        mRecyclerView.adapter = adapter
     }
 
     inner class MyAdapter(private val data: MutableList<String>) :
         RecyclerView.Adapter<MyViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-            val view = LayoutInflater.from(this@MainActivity)
+            val view = LayoutInflater.from(this@NestedActivity)
                 .inflate(R.layout.item_nested_list, parent, false)
             return MyViewHolder(view)
         }
