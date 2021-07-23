248936f5d48bb7a0b88a0952492e0b7791063dd4
陈扬齐 <766790183@qq.com>
2021-07-23 11:38:46 +0800

    feat:简单的自定义Behavior

--- NestedScroll/app/src/main/java/com/chenyangqi/nested/scroll/demo3/Demo3RecycleviewBehavior.kt
+++ NestedScroll/app/src/main/java/com/chenyangqi/nested/scroll/demo3/Demo3RecycleviewBehavior.kt
@@ -3,10 +3,9 @@ package com.chenyangqi.nested.scroll.demo3
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.widget.RelativeLayout
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.ViewCompat
-
-
 
 
 /**
@@ -17,5 +16,19 @@ import androidx.core.view.ViewCompat
 class Demo3RecycleviewBehavior<V : View>(context: Context, attributeSet: AttributeSet?) :
     CoordinatorLayout.Behavior<V>(context, attributeSet) {
 
+    override fun layoutDependsOn(parent: CoordinatorLayout, child: V, dependency: View): Boolean {
+        return dependency is RelativeLayout
+    }
+
+    override fun onDependentViewChanged(
+        parent: CoordinatorLayout,
+        child: V,
+        dependency: View
+    ): Boolean {
+        var translationY: Float = dependency.measuredHeight - dependency.translationY
+        translationY = if (translationY < 0) 0F else translationY
+        child.y = translationY
+        return true
+    }
 
 }
