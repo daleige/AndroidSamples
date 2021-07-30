33be8ab6f6aa68646bc513df7cfa3f3f094454cc
陈扬齐 <766790183@qq.com>
2021-07-30 15:04:52 +0800

    feat:基本的compose布局

--- ComposeDemo/app/src/main/java/com/chenyangqi/composedemo/MainActivity.kt
+++ ComposeDemo/app/src/main/java/com/chenyangqi/composedemo/MainActivity.kt
@@ -3,36 +3,60 @@ package com.chenyangqi.composedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import com.chenyangqi.composedemo.ui.theme.ComposeDemoTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            DefaultPreview()
+        }
+    }
+
+    @Preview(showSystemUi = true, showBackground = true, backgroundColor = 0xFFFFFFFF)
+    @Composable
+    fun DefaultPreview() {
+        MaterialTheme() {
+            val typography = MaterialTheme.typography
+            Column(Modifier.padding(20.dp)) {
+                Image(
+                    painter = painterResource(id = R.drawable.header),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(200.dp)
+                        .fillMaxWidth()
+                        .clip(shape = RoundedCornerShape(4.dp)),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    "A day wandering through the sandhills " +
+                            "in Shark Fin Cove, and a few of the " +
+                            "sights I saw",
+                    style = typography.h6,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text("Davenport, California", style = typography.body2)
+                Text("December 2018", style = typography.body2)
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeDemoTheme {
-        Greeting("Android")
+    @Composable()
+    fun NewsStory(name: String) {
+        Text("Hell $name")
     }
 }
