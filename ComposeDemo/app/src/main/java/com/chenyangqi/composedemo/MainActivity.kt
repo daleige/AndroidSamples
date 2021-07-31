dac1f26ce0170e33e8ec8f5f6edbecd52d596fab
Chenyangqi-Mac <cyq18569640023@gmail.com>
2021-07-31 23:55:56 +0800

    feat:compose基础布局学习

--- ComposeDemo/app/src/main/java/com/chenyangqi/composedemo/MainActivity.kt
+++ ComposeDemo/app/src/main/java/com/chenyangqi/composedemo/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
