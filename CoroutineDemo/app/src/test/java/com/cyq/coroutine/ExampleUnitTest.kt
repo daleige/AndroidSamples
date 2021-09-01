236aef6143b8addd4e218503c795cb3c8c274cc5
陈扬齐 <766790183@qq.com>
2021-09-01 18:58:42 +0800

    feat：lifecycScoble避免协程内存泄漏

--- CoroutineDemo/app/src/test/java/com/cyq/coroutine/ExampleUnitTest.kt
+++ CoroutineDemo/app/src/test/java/com/cyq/coroutine/ExampleUnitTest.kt
@@ -1,5 +1,8 @@
 package com.cyq.coroutine
 
+import android.util.Log
+import dalvik.annotation.TestTarget
+import kotlinx.coroutines.*
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +16,86 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun main() {
+        println("GlobalScope begin...")
+        GlobalScope.launch {
+            println("GlobalScope running...")
+        }
+        Thread.sleep(1000)
+        println("GlobalScope end...")
+    }
+
+    @Test
+    fun testRunBlocking() {
+        //runBlocking会阻塞当前线程，知道作用域执行结束后释放
+        runBlocking {
+            launch {
+                println("part 1 doing")
+                delay(2000)
+                println("part 1 complete")
+            }
+            launch {
+                println("part 2 doing")
+                delay(1000)
+                println("part 2 complete")
+            }
+        }
+        println("do finally...")
+    }
+
+    @Test
+    fun testLaunch() {
+        runBlocking {
+            launch {
+                println("part 1 doing")
+                delay(2000)
+                println("part 1 complete")
+            }
+            launch {
+                println("part 2 doing")
+                delay(1000)
+                println("part 2 complete")
+            }
+            println("do finally...")
+        }
+    }
+
+    /**
+     * async它会创建一个新的子协程并返回一个Deferred对象，如果我们想要获取async函数代码块的执行结果，只需要调用Deferred对象的await()方法即可
+     */
+    @Test
+    fun testAsync() {
+        runBlocking {
+            val a = 8
+            val step1 = async {
+                println("async 1先执行")
+                a * 2
+            }
+            val step2 = async {
+                println("async 2后执行")
+                step1.await() * 3
+            }
+            println("final result = ${step2.await()}")
+        }
+    }
+
+    @Test
+    fun testCoroutineScope() {
+        suspend fun f() = coroutineScope {
+            launch {
+                test()
+            }
+        }
+        GlobalScope.launch {
+            f()
+        }
+        Thread.sleep(1000)
+    }
+
+    private fun test() {
+        println("----------")
     }
 }
