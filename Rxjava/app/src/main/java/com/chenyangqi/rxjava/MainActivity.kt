ef1953bfef1d38156cf7af533e654036c1cd924e
Chenyangqi-Mac <cyq18569640023@gmail.com>
2021-10-23 23:19:14 +0800

    feat:create操作符

--- Rxjava/app/src/main/java/com/chenyangqi/rxjava/MainActivity.kt
+++ Rxjava/app/src/main/java/com/chenyangqi/rxjava/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.chenyangqi.rxjava
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.chenyangqi.rxjava.创建型操作符.CreateTypeActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnCreateType.setOnClickListener {
+            startActivity(Intent(this, CreateTypeActivity::class.java))
+        }
     }
 }
