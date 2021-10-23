43ca4db45cf2dbef19d38d07a5ccedd83f9071ac
Chenyangqi-Mac <cyq18569640023@gmail.com>
2021-10-23 22:11:02 +0800

    feat:创建rxjava

--- Rxjava/app/src/main/java/com/chenyangqi/rxjava/MainActivity.kt
+++ Rxjava/app/src/main/java/com/chenyangqi/rxjava/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
     }
 }
